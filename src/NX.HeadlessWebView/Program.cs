6bd0059d0eba6bc1624bda453397b568b0fab1b1
noxi515 <tzo.ppl@gmail.com>
2023-07-16 17:36:41 +0900

    とりあえず作った

---
+++ src/NX.HeadlessWebView/Program.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NX.HeadlessWebView;
+
+await Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<WebViewHost>();
+    })
+    .RunConsoleAsync();
---
+++ src/NX.HeadlessWebView/WebViewHost.cs
@@ -0,0 +1,59 @@
+﻿using System.Windows.Threading;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Web.WebView2.Core;
+
+namespace NX.HeadlessWebView;
+
+public class WebViewHost : IHostedService
+{
+    private static readonly IntPtr HWND_MESSAGE = new(-3);
+
+    private readonly CoreWebView2Controller _controller;
+    private readonly Dispatcher _dispatcher;
+
+    public WebViewHost()
+    {
+        var dispatcherSource = new TaskCompletionSource<Dispatcher>();
+        var controllerSource = new TaskCompletionSource<CoreWebView2Controller>();
+
+        // UI用のスレッドを作成してWebView2Controllerの生成
+        var uiThread = new Thread(() =>
+        {
+            var dispatcher = Dispatcher.CurrentDispatcher;
+            dispatcherSource.SetResult(dispatcher);
+
+            dispatcher.Invoke(async () =>
+            {
+                // WebView2Controllerの生成
+                var env = await CoreWebView2Environment.CreateAsync();
+                var controller = await env.CreateCoreWebView2ControllerAsync(HWND_MESSAGE);
+                controllerSource.SetResult(controller);
+            });
+            Dispatcher.Run();
+        });
+
+        // UIスレッドとして実行
+        uiThread.SetApartmentState(ApartmentState.STA);
+        uiThread.Start();
+
+        // 作成したDispatcherとWebView2Controllerをフィールドに保持
+        _dispatcher = dispatcherSource.Task.Result;
+        _controller = controllerSource.Task.Result;
+    }
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        // WebView2Controllerの破棄（しているつもりなのだが、エラーが発生しているのか終了しない）
+        _dispatcher.Invoke(() =>
+        {
+            _controller.Close();
+        });
+
+        return Task.CompletedTask;
+    }
+}
